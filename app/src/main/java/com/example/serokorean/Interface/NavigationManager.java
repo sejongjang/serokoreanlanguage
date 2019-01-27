bb49dee881c7a8961e5412dcd6b5700207a07a77
Matt <matt@serolanguage.com>
2019-01-27 16:59:41 -0700

    deleted unnecessary files

--- app/src/main/java/com/example/serokorean/Interface/NavigationManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.serokorean.Interface;
-
-import java.util.List;
-
-public interface NavigationManager {
-//    void showFragment(String title);
-
-    void showFragment(String selectedItem, String s);
-}
