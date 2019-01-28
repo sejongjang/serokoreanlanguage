c1d108fec5cb68565a28020c0c15048b8863140b
Matt <matt@serolanguage.com>
2019-01-27 19:19:11 -0700

    added page 6

--- app/src/main/java/com/example/serokorean/temp/ChapterPages.java
+++ app/src/main/java/com/example/serokorean/temp/ChapterPages.java
@@ -68,6 +68,7 @@ public class ChapterPages extends AppCompatActivity implements GestureDetector.O
 
         webView = findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 //        webView.getSettings().setBuiltInZoomControls(true);
         webView.loadUrl(htmlFiles.get(0));
 
