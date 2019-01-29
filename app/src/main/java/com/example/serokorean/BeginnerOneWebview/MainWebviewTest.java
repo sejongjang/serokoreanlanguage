e76b7c4484531a9738e11b19fb9b86aad6e5b943
sejong <youthrich40@gmail.com>
2019-01-29 03:10:35 -0700

    update UI

--- app/src/main/java/com/example/serokorean/BeginnerOneWebview/MainWebviewTest.java
+++ app/src/main/java/com/example/serokorean/BeginnerOneWebview/MainWebviewTest.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
